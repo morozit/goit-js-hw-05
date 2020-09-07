1c5e1665a1452c733e26703eee495f68e54ae4c4
Roman <65686226+morozit@users.noreply.github.com>
2020-09-07 17:03:13 +0300

    add 5task

---
+++ js/task/task-5-1.js
@@ -0,0 +1,12 @@
+// Write code under this line
+
+//console.log(typeof Account.prototype.getInfo);
+// 'function'
+
+// const mango = new Account( 'Mangozedog', 'mango@dog.woof');
+//console.log(mango.getInfo());
+// 'login : Mangozedog, email: mango@dog.woof'
+
+// const poly = new Account( 'Poly', 'poly@mail.com');
+//console.log(poly.getInfo());
+// 'login : Poly, email: poly@mail.com'
