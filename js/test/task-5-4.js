9c45579ab4aedb48af03ec4b44e4faa344a8bb9f
Roman <65686226+morozit@users.noreply.github.com>
2020-09-09 01:04:13 +0300

    Update task-5-4.js

---
+++ js/test/task-5-4.js
@@ -0,0 +1,15 @@
+// Write code under this line
+
+// console.log(typeof StringBuilder);
+// 'function'
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
